dd63ba717f76ae4c87bb1015ec759cc279238827
fullcrimp <fullcrimp@protonmail.com>
2017-10-23 23:29:24 +0300

    add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,22 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min
+        this.max = max
+        this.lastGuess
     }
 
     guess() {
-
+        this.lastGuess = this.min + Math.ceil((this.max - this.min) / 2)
+        return this.lastGuess
     }
 
     lower() {
-
+        this.max = this.lastGuess
     }
 
     greater() {
-
+        this.min = this.lastGuess        
     }
 }
 
